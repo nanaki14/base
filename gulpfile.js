ce48286b6dd9b95003834062ed6257200066ec06
nanaki14 <masa@masa.local>
2017-12-22 16:04:29 +0900

    👷 gulp sass ejs
    gulp
    add html-beautify
    add ignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,27 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');
+const gulp         = require('gulp');
+const sass         = require('gulp-sass');
+const sourcemaps   = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
-const imagemin = require("gulp-imagemin");
-const mozjpeg = require('imagemin-mozjpeg');
-const pngquant = require('imagemin-pngquant');
-const gifsicle = require('imagemin-gifsicle');
-const svgo = require('imagemin-svgo');
-const uglify = require('gulp-uglify');
-const browserify = require('gulp-browserify');
-const babel = require('gulp-babel');
-const plumber = require("gulp-plumber");
-const ejs = require('gulp-ejs');
-const notify = require('gulp-notify');
-const changed = require('gulp-changed');
-const browserSync = require('browser-sync').create();
+const imagemin     = require("gulp-imagemin");
+const mozjpeg      = require('imagemin-mozjpeg');
+const pngquant     = require('imagemin-pngquant');
+const gifsicle     = require('imagemin-gifsicle');
+const svgo         = require('imagemin-svgo');
+const uglify       = require('gulp-uglify');
+const browserify   = require('gulp-browserify');
+const babel        = require('gulp-babel');
+const plumber      = require("gulp-plumber");
+const ejs          = require('gulp-ejs');
+const htmlbeautify = require('gulp-html-beautify');
+const notify       = require('gulp-notify');
+const changed      = require('gulp-changed');
+const ignore       = require('gulp-ignore');
+const browserSync  = require('browser-sync').create();
 
 const baseDir = {
   dest: 'dist',
   sass: 'src/**/*.scss',
   js: 'src/**/*.js',
-  ejs: ['src/**.ejs','src/**/*.ejs'],
   img: 'src/**/*.{png,jpg,gif,svg}'
 }
 
@@ -49,14 +50,19 @@ gulp.task('sass', () => {
 
 gulp.task('ejs', () => {
   return gulp.src([
-      baseDir.ejs,
+      'src/**.ejs',
+      'src/**/*.ejs',
       '!' + 'src/**/_*.ejs'
     ])
+    .pipe(plumber())
     // .pipe(changed(baseDir.dest))
     .pipe(ejs({}, {}, {
       'ext': '.html'
     }))
-    .pipe(plumber())
+    .pipe(htmlbeautify({
+      indent_size: 2,
+      max_preserve_newlines: 1
+    }))
     .pipe(gulp.dest(baseDir.dest))
     .pipe(browserSync.stream());
 });
@@ -100,6 +106,7 @@ gulp.task('copy', () => {
       '!src/**/*.ejs',
       '!src/*.+(jpg|png|gif|svg)'
     ])
+    .pipe(ignore.include({isFile: true}))
     .pipe(gulp.dest(baseDir.dest))
 });
 
@@ -111,7 +118,7 @@ gulp.task('watch', () => {
   });
 
   gulp.watch([baseDir.sass], ['sass']);
-  gulp.watch([baseDir.ejs], ['ejs']);
+  gulp.watch(['src/*.ejs','src/**/*.ejs'], ['ejs']);
   gulp.watch([baseDir.js], ['babel']);
   gulp.watch([baseDir.img], ['imagemin']);
   gulp.watch([
--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,6 +1,32 @@
 const $ = require("jquery");
 const easing = require("jquery.easing");
 
+const _ua = ((u) => {
+  return {
+    Tablet:(u.indexOf("windows") != -1 && u.indexOf("touch") != -1 && u.indexOf("tablet pc") == -1)
+      || u.indexOf("ipad") != -1
+      || (u.indexOf("android") != -1 && u.indexOf("mobile") == -1)
+      || (u.indexOf("firefox") != -1 && u.indexOf("tablet") != -1)
+      || u.indexOf("kindle") != -1
+      || u.indexOf("silk") != -1
+      || u.indexOf("playbook") != -1,
+    Mobile:(u.indexOf("windows") != -1 && u.indexOf("phone") != -1)
+      || u.indexOf("iphone") != -1
+      || u.indexOf("ipod") != -1
+      || (u.indexOf("android") != -1 && u.indexOf("mobile") != -1)
+      || (u.indexOf("firefox") != -1 && u.indexOf("mobile") != -1)
+      || u.indexOf("blackberry") != -1
+  }
+})(window.navigator.userAgent.toLowerCase());
+
+const g = {
+  isSp:function(){ return ( g.winW() <= g.point )? true : false ; },// SP or PC
+  winW:function(){ return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth; },
+  winH:function(){ return $( window ).height(); },
+  winT:function(){ return $( window ).scrollTop(); },
+  point: 768
+}
+
 let Common = function(){};
 
 $(function() {
